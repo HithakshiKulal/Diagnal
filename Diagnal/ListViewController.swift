02b385e8640842c88cc6df8a4838bbff2ff332e4
HithakshiKulal <hithakshikulal@gmail.com>
2022-03-04 15:23:43 +0530

    Initial commit

---
+++ Diagnal/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListViewController.swift
+//  Diagnal
+//
+//  Created by Hithakshi on 04/03/22.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
